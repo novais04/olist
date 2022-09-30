82e48a9fbe47bfa178567dcfad05048a02d8d35e
anselmo <novais03@gmail.com!>
2022-09-30 18:48:03 -0300

    Criaindo banco de dados_categorias

--- src/python/upload_data.py
+++ src/python/upload_data.py
@@ -1,43 +1,24 @@
-
 import os
 import pandas as pd
 import sqlalchemy
 
-
-
 str_conection = 'sqlite:///{path}'
 
 # Os endereços de nosso prjeto e sub pastas
-BASE_DIR = os.path.dirname( os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname( os.path.dirname(os.path.abspath(__file__))))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
-"""
-print(f"Meu diretório de projeto é: {BASE_DIR}")
-print(f"Meu diretório dos dados é: {DATA_DIR}")
-
-
-file_names = os.listdir(DATA_DIR)
-correct_files = []
-
-#forma 1
-for i in file_names:
-    if i.endswith(".csv"):
-        correct_files.append(i)
-"""
-    
 
 # Encontrando os arquivo de dados
 file_names = [i for i in os.listdir(DATA_DIR) if i.endswith(".csv")]
+print(file_names)  
 
 # Arbindo conexão com o banco...
 str_conection = str_conection.format(path=os.path.join(DATA_DIR,'olist.db'))
 connection = sqlalchemy.create_engine( str_conection)
 
-# Para cada arquivo realiza a inserção dos dados no banco
 for i in file_names:
     print(i)
     df_temp = pd.read_csv(os.path.join(DATA_DIR, i))
     table_name = "tb_" + i.strip(".csv").replace("olist_","").replace("_dataset","")
-    df_temp.to_sql(table_name, connection)
-                          
-
-
+    df_temp.to_sql(table_name,con=connection,if_exists='replace',index= False)
+    
---
+++ src/sql/analise.sql
@@ -0,0 +1,48 @@
+select t1.*,
+    CASE WHEN   pct_receita <= 0.5 and pct_freq <= 0.5 then "BAIXO VALOR"
+    WHEN        pct_receita > 0.5  and pct_freq <= 0.5 then "ALTO VALOR"
+    WHEN        pct_receita <= 0.5 and pct_freq > 0.5  then "ALTA FREQ"
+    WHEN        pct_receita < 0.9  or pct_freq < 0.9  then "PRODUTIVO"
+    ELSE        "SUPER PRODUTIVO"
+    END AS semento_valor_freq
+from
+(
+    select t1.*,
+        percent_rank() OVER (order by receita_total asc) as pct_receita, 
+        PERCENT_rank() over (order by qtde_pedidos asc) as pct_freq
+
+    from(
+
+        select
+            t2.seller_id,
+            sum(t2.price) as receita_total,
+            count(DISTINCT t1.order_id) as qtde_pedidos,
+            count(t2.product_id) as qtde_produtos,
+            count(DISTINCT t2.product_id) as qtde_Uniq_produtos,
+            min(julianday('2018-06-01') - julianday(t1.order_approved_at)) as qt_dias_ult_venda,
+            max(cast(julianday('2018-06-01')- julianday(t3.dt_inicio) as int)) as qtde_dias_base
+
+        from tb_orders as t1
+        left join tb_order_items as t2
+        on t1.order_id = t2.order_id
+
+        left join ( 
+            select 
+                t2.seller_id,
+                min( date(t1.order_approved_at)) as dt_inicio
+            from tb_orders as t1
+            left join tb_order_items as t2
+            on t1.order_id = t2.order_id
+
+            group by t2.seller_id 
+        ) as t3
+        on t2.seller_id = t3.seller_id
+
+        where t1.order_approved_at BETWEEN '2017-06-01' and '2018-06-01'
+
+        group by t2.seller_id
+    ) as t1
+) as t1
+
+
+
